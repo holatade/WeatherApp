9f49bd84734e50a3f090d5232914f86f2a3b6663
Hassan Hassan <hassan.olatade.hh@gmail.com>
2022-06-26 15:12:00 +0100

    Removed Unessary consle.log call

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-console.log('Client side javascript')
-
 const getForecast = (address,callback) => fetch(`http://localhost:3000/weather?address=${address}`)
 .then((response) => {
     response.json().then((data) => {
